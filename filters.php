dea06b49ad7c3640f1c1a6e417618ae7399c7c19
xLink <xLink@cybershade.org>
2014-08-23 17:28:34 +0100

    overloaded the admin auth filter, to ensure that the irc cookie is active at all times

--- filters.php
+++ filters.php
@@ -8,5 +8,23 @@ Route::filter('auth.user', function () {
     }
 });
 
+Route::filter('auth.admin', function () {
+    if (Auth::guest()) {
+        Session::flash('info', 'You need to login');
+
+        return Redirect::guest(URL::route('pxcms.user.login'));
+    }
+
+    if (Cookie::get('darchoods_token', null) === null) {
+        Session::flash('info', 'You complete that action, you need to reauthenticate.');
+
+        return Redirect::guest(URL::route('pxcms.user.login'));
+    }
+
+    if (Auth::user()->isAdmin() === false) {
+        return Redirect::route('pxcms.pages.home')->withError(Lang::get('admin::admin.unauthorized'));
+    }
+});
+
 // Check permissions when we start in the admin panel
 Route::when(Config::get('core::routes.paths.user').'/*', 'auth.user');
